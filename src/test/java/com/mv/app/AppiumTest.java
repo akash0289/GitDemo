bf1c7daa16a48335f46f8904c3472b2c458f3b76
akash0289 <akashshri171@gmail.com>
2020-02-17 11:58:00 +0530

    my first commit

---
+++ src/test/java/com/mv/app/AppiumTest.java
@@ -0,0 +1,18 @@
+package com.mv.app;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void nativeApp()
+	{
+		System.out.println("nativeApp");
+	}
+	
+	@Test
+	public void hybridApp()
+	{
+		System.out.println("hybridApp");
+	}
+}
